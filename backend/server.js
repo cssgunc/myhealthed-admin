6767b3fdfcd6d560d1486acd58603d870047f011
mkirby528 <mkirby528@unc.edu>
2020-02-13 19:34:25 -0500

    begin making story table

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,50 @@ const sequelize = new Sequelize('myhealthed', 'postgres', '052899', {
 
 sequelize.authenticate().then(() => {
   console.log("Success!");
+  var Stories = sequelize.define('stories', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    perspective: {
+      type: Sequelize.STRING
+    },
+    age:{
+        type: Sequelize.INTEGER,
+    },
+    lgbtq:{
+        type: Sequelize.STRING
+    },
+    race:{
+        type: Sequelize.STRING
+    },
+    phone:{
+        type:Sequelize.STRING
+    },
+    topic:{
+        type: Sequelize.STRING
+    },
+    title:{
+        type: Sequelize.STRING
+    },
+    published:{
+        type: Sequelize.DATE
+    }
+
+
+
+  },
+  {
+    freezeTableName: true
+  });
+
+  Posts.sync({force: true}).then(function () {
+    return Posts.create({
+      title: 'Getting Started with PostgreSQL and Sequelize',
+      content: 'Hello there'
+    });
+  });
 }).catch((err) => {
   console.log(err);
 });
