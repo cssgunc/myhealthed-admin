7ee68681386ce7b6721f55a177a97d604cf9eb64
mkirby528 <mkirby528@unc.edu>
2020-03-21 17:05:13 -0400

    add dummy secrets file and git ignore real secrets

---
+++ backend/config/secrets_example.js
@@ -0,0 +1,14 @@
+/*
+ * Rename this file to secrets.js so that it will be gitignored and found by server.js, then enter the information below.
+ *
+*/
+
+
+module.exports = {
+    database:{
+        db: /* database name goes here */ , 
+        user: /* username goes here */,
+        password: /* password goes here */
+    }
+}
+
