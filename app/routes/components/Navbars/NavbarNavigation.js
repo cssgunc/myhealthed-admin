a6ac9239fc81ea55f5a3481a1008ec8339d12961
Austin Wang <austin.wang0311@gmail.com>
2020-03-25 16:42:05 -0400

    removed some unneeded routes and deps

--- app/routes/components/Navbars/NavbarNavigation.js
+++ app/routes/components/Navbars/NavbarNavigation.js
@@ -64,58 +64,6 @@ const NavbarNavigation = ({ accent, pills, ...navbarProps }) => (
             </DropdownMenu>
         </UncontrolledDropdown>
 
-        <NestedDropdown nav inNavbar>
-            <DropdownToggle nav>
-                Apps
-                <i className="fa fa-angle-down fa-fw ml-1"></i>
-            </DropdownToggle>
-            <DropdownMenu>
-                <NestedDropdown.Submenu title="Projects">
-                    <DropdownItem tag={ Link } to="/apps/projects/list">Projects List</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/projects/grid">Projects Grid</DropdownItem>
-                </NestedDropdown.Submenu>
-                <NestedDropdown.Submenu title="Tasks">
-                    <DropdownItem tag={ Link } to="/apps/tasks/list">Tasks List</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/tasks/grid">Tasks Grid</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/tasks/kanban">Tasks Kanban</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/tasks/details">Task Details</DropdownItem>
-                </NestedDropdown.Submenu>
-                <NestedDropdown.Submenu title="Files">
-                    <DropdownItem tag={ Link } to="/apps/files/list">Files List</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/files/grid">Files Grid</DropdownItem>
-                </NestedDropdown.Submenu>
-                <NestedDropdown.Submenu title="Search Results">
-                    <DropdownItem tag={ Link } to="/apps/search-results">Search Results</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/images-results">Images Results</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/videos-results">Videos Results</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/users-results">Users Results</DropdownItem>
-                </NestedDropdown.Submenu>
-                <NestedDropdown.Submenu title="Users">
-                    <DropdownItem tag={ Link } to="/apps/users/list">Users List</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/users/grid">Users Grid</DropdownItem>
-                </NestedDropdown.Submenu>
-                <NestedDropdown.Submenu title="Gallery">
-                    <DropdownItem tag={ Link } to="/apps/gallery-grid">Gallery Grid</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/gallery-table">Gallery Table</DropdownItem>
-                </NestedDropdown.Submenu>
-                <NestedDropdown.Submenu title="Mailbox">
-                    <DropdownItem tag={ Link } to="/apps/inbox">Inbox</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/new-email">New Email</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/email-details">Email Details</DropdownItem>
-                </NestedDropdown.Submenu>
-                <NestedDropdown.Submenu title="Profile">
-                    <DropdownItem tag={ Link } to="/apps/profile-details">Profile Details</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/profile-edit">Profile Edit</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/account-edit">Account Edit</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/billing-edit">Billing Edit</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/settings-edit">Settings Edit</DropdownItem>
-                    <DropdownItem tag={ Link } to="/apps/sessions-edit">Sessions Edit</DropdownItem>
-                </NestedDropdown.Submenu>
-                <DropdownItem tag={ Link } to="/apps/clients">Clients</DropdownItem>
-                <DropdownItem tag={ Link } to="/apps/chat">Chat</DropdownItem>
-            </DropdownMenu>
-        </NestedDropdown>
-
         <UncontrolledDropdown nav inNavbar>
             <DropdownToggle nav>
                 Layouts                                 
