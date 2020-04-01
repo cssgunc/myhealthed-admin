d0977d449e6ba07c8ebd75d186bc2b25ce403217
Austin Wang <austin.wang0311@gmail.com>
2020-04-01 04:32:46 -0400

    Readd separate backend server, create new command to run webpack dev server

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cors = require('cors')
+const cookieParser = require("cookie-parser");
+const db = require("./config/database");
+
+const app = express();
+
+app.use(cors())
+app.options('*', cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser()); // required before session.
+app.set("json spaces", 2);
+
+app.use(
+    session({
+      secret: "sfsalfnsaflknsadlknsandlsanfsanfnsaldnsadnlsalndsaldlnsadlnsandl",
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+
+const API_PORT = process.env.API_PORT || 3001;
+
+
+
+db.sequelize.sync({}).then(() => console.log("Database connected"));
+
+
+app.use("/api/stories", require("./routes/stories"));
+app.use("/api/links",require("./routes/links"))
+
+app.listen(API_PORT, () => console.log(`API listening on port ${API_PORT}`));
