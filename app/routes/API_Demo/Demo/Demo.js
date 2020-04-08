b120db6e66d79f68abe923b444f995cb2ad4d3c9
jasontyu <jasonyu6154@gmail.com>
2020-04-08 17:57:29 -0400

    Cleanup APIDemo

--- app/routes/API_Demo/Demo/Demo.js
+++ app/routes/API_Demo/Demo/Demo.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import {Container} from './../../../components';
+import { Container } from './../../../components';
 
 const Demo = () => {
   const [page, setPage] = useState(0);
   const [stories, setStories] = useState([]);
-  
+
   useEffect(() => {
     fetchData();
   },[]);
@@ -12,26 +12,23 @@ const Demo = () => {
   async function fetchData() {
     let url = `/api/stories/get?page=${page}`;
     console.log(url);
-    const res = await fetch(url);
-    res
-      .json()
-      .then(data => setStories(stories.concat(data)))
-      .then(()=> setPage(page+1))
-      .catch(err => console.log(err));
-  }
-
-  const loadMore = () =>{
-      fetchData();
+    fetch(url)                           // returns Promise of response
+      .then(response => response.json()) // transform response into JS object
+      .then(data => {
+        setStories(stories.concat(data));// update component with new data
+        setPage(page+1);                 // increment page counter
+    })                                   // (so next call will fetch next page)
+      .catch(err => { console.log(err); });
   }
 
   return (
     <Container>
       <div>API TEST PAGE</div>
-      <button onClick={loadMore}>Load More</button>
+      <button onClick={ fetchData }>Load More</button>
       <div>
-        {stories.map((story, i) => {
-          return <p key={i}>{story.id} - {story["story texts"]}</p>;
-        })};
+        { stories.map((story, i) => {
+          return <p key={ i }>{ story.id } - { story["story texts"] }</p>;
+        }) };
       </div>
     </Container>
   );
