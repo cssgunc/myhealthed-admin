64d6d26ccff2174da566203035c9f340feaf13df
Austin Wang <austin.wang0311@gmail.com>
2020-04-13 16:33:16 -0400

    Removed unused routes and components, removed unused deps from server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require("express");
+const db = require("./backend/db/database");
+
 const app = express();
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static('dist'));
+app.set("json spaces", 2);
+
+db.sequelize.sync({}).then(() => console.log("Database connected"));
+
+// links routes for backend api
+app.use("/api/stories", require("./backend/routes/stories"));
+app.use("/api/links", require("./backend/routes/links"));
+
+// links routes for frontend React webpack build
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
